c348f8356211982dcb9961458cf05dcbb5882581
yusufarifadillah <150007564+yusufarifadillah@users.noreply.github.com>
2024-03-23 02:31:22 +0700

    update 3 master data

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\BahanbakuController;
-use App\Http\Controllers\ContohformController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\MenuController;
 use App\Http\Controllers\PelangganController;
 
 /*
@@ -33,15 +34,21 @@ Route::get('/dashboardbootstrap', function () {
 
 // route untuk validasi login
 Route::post('/validasi_login', [App\Http\Controllers\LoginController::class, 'show']);
+// route supplier
+Route::resource('/supplier', SupplierController::class)->middleware(['auth']);
+Route::get('/supplier/destroy/{id}', [App\Http\Controllers\SupplierController::class,'destroy'])->middleware(['auth']);
+// route Pegawai
+Route::resource('/pegawai', PegawaiController::class)->middleware(['auth']);
+Route::get('/pegawai/destroy/{id}', [App\Http\Controllers\PegawaiController::class,'destroy'])->middleware(['auth']);
+// route Bahan Baku
+Route::resource('/bahanbaku', BahanbakuController::class)->middleware(['auth']);
+Route::get('/bahanbaku/destroy/{id}', [App\Http\Controllers\BahanbakuController::class,'destroy'])->middleware(['auth']);
+// route Menu
+Route::resource('/menu', MenuController::class)->middleware(['auth']);
+Route::get('/menu/destroy/{id}', [App\Http\Controllers\MenuController::class,'destroy'])->middleware(['auth']);
+// route Pelanggan
+Route::resource('/pelanggan', PelangganController::class)->middleware(['auth']);
+Route::get('/pelanggan/destroy/{id}', [App\Http\Controllers\PelangganController::class,'destroy'])->middleware(['auth']);
 
-// route selamat
-Route::get('/selamat', function () {
-    return view('selamat',['nama'=>'Hendro Jokondo-kondo']);
-});
-
-// route contoh1
-Route::get('/contoh1', [App\Http\Controllers\Contoh1Controller::class, 'show']);
-// route contoh2
-Route::get('/contoh2', [App\Http\Controllers\Contoh2Controller::class, 'show']);
 
 require __DIR__.'/auth.php';
