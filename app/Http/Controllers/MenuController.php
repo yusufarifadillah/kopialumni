f9f24cfca6fbb0161c7f7711f7bb114282632ddb
lulufaridah <lulufaridah04@gmail.com>
2024-03-23 21:53:18 +0700

    masterdata menu

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Menu;
+use App\Http\Requests\StoreMenuRequest;
+use App\Http\Requests\UpdateMenuRequest;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //query data
+        $menu = Menu::all();
+        return view('menu.view',
+                    [
+                        'menu' => $menu
+                    ]
+                  );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // berikan kode menu secara otomatis
+        // 1. query dulu ke db, select max untuk mengetahui posisi terakhir 
+        
+        return view('menu/create',
+                    [
+                        'kode_menu' => Menu::getKodeMenu()
+                    ]
+                  );
+        // return view('menu/view');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreMenuRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreMenuRequest $request)
+    {
+        //digunakan untuk validasi kemudian kalau ok tidak ada masalah baru disimpan ke db
+        $validated = $request->validate([
+            'kode_menu' => 'required',
+            'makanan' => 'required|unique:menu|min:5|max:255',
+            'minuman' => 'required|unique:menu|min:5|max:255',
+            'harga' => 'required',
+        ]);
+
+        // masukkan ke db
+        Menu::create($request->all());
+        
+        return redirect()->route('menu.index')->with('success','Data Berhasil di Input');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Menu  $menu
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Menu $menu)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Menu  $menu
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Menu $menu)
+    {
+        return view('menu.edit', compact('menu'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateMenuRequest  $request
+     * @param  \App\Models\Menu  $menu
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateMenuRequest $request, Menu $menu)
+    {
+        //digunakan untuk validasi kemudian kalau ok tidak ada masalah baru diupdate ke db
+        $validated = $request->validate([
+            'kode_menu' => 'required',
+            'makanan' => 'required|max:255',
+            'minuman' => 'required|max:255',
+            'harga' => 'required',
+        ]);
+    
+        $menu->update($validated);
+    
+        return redirect()->route('menu.index')->with('success','Data Berhasil di Ubah');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Menu  $menu
+     * @return \Illuminate\Http\Response
+     */
+    // public function destroy(Menu $menu)
+    public function destroy($id)
+    {
+        //hapus dari database
+        $menu = Menu::findOrFail($id);
+        $menu->delete();
+
+        return redirect()->route('menu.index')->with('success','Data Berhasil di Hapus');
+    }
+}
